38825e43f4a922f9d9faececd67db8deddf89c84
Harrison Sabean <hsabes@gmail.com>
2022-05-18 08:01:51 -0700

    commit tree 1a9b6222b960edd15b5a79b054396d712b8c5cc1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(defaultActivity = 'roller-skate'){
+    return `This Saturday, I want to ${defaultActivity}!`;
+}
+
+let mondayWork = function (defaultActivity = 'go to the office'){
+    return `This Monday, I will ${defaultActivity}.`;
+}
+
+let wrapAdjective = function (wrap = '*'){
+    return function (message = 'a hard worker'){
+        return `You are ${wrap}${message}${wrap}!`
+    }
+}
